ff31364907eddeb8a0bf5791f8e71a0013f61829
yanic <yanic.dev@gmail.com>
2026-02-16 18:39:57 +0100

    keybind: fix netrw keybind syntax

--- lua/btw/keybinds.lua
+++ lua/btw/keybinds.lua
@@ -2,4 +2,4 @@ local set_keybind = vim.keymap.set
 local builtin = require('telescope.builtin')
 
 set_keybind('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-set_keybind('n', '<leader>e', ':Ex<CR>', { norempa = true, silent = true })
+set_keybind('n', '<leader>e', vim.cmd.Ex)
