0af79861ae2d8327581eebb19a98e2c63a1d9ee4
Doepner <yanic.dev@gmail.com>
2026-02-18 11:58:41 +0100

    plugin: add runner

--- lua/btw/keybinds.lua
+++ lua/btw/keybinds.lua
@@ -1,6 +1,7 @@
 local set_keybind = vim.keymap.set
 local builtin = require('telescope.builtin')
 
-set_keybind('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 set_keybind('n', '<leader>e', vim.cmd.Ex)
+set_keybind('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+set_keybind('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
 set_keybind("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
---
+++ lua/plugins/runner.lua
@@ -0,0 +1,4 @@
+return {
+    "TheLazyCat00/runner-nvim",
+    opts = {}
+}
