542a2add800e3960054e1f8a9c73e059dcb4e0d4
yanic <yanic.dev@gmail.com>
2026-02-23 18:06:06 +0100

    lsp: add c, cpp and rust lsp

--- lua/btw/lsp.lua
+++ lua/btw/lsp.lua
@@ -1,7 +1,20 @@
-vim.lsp.config('luals', {
+vim.lsp.config('lua', {
   cmd = {'lua-language-server'},
   filetypes = {'lua'},
   root_markers = {'.luarc.json', '.luarc.jsonc'},
 })
 
-vim.lsp.enable('luals')
+vim.lsp.config('rust', {
+  cmd = {'rust-analyzer'},
+  filetypes = {'rust'},
+  root_markers = {'Cargo.toml'},
+})
+
+vim.lsp.config('cpp', {
+  cmd = {'clangd'},
+  filetypes = {'cpp', 'c'},
+  root_markers = { 'CMakeLists.txt', 'Makefile' },
+})
+
+
+vim.lsp.enable({ 'lua', 'rust', 'cpp' })
